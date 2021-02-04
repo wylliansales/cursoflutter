8475e65defc466602684338c2b38ac2700cf65e1
Wyllian Sales <wylliansalles@gmail.com>
2021-02-04 06:35:03 -0300

    t

---
+++ lib/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/cupertino.dart';
+
+const KPrimaryColor = Color(0xFF0C9869);
+const KTextColor = Color(0xFF3C4046);
+const KBackgroundColor = Color(0xFFF9F8FD);
+
+const double kDefaultPadding = 20.0;
